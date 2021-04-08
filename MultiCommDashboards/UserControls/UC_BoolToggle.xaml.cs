d953f746530f7d76d56f78989a101d31f656bc13
michael@roop.ca <michael@roop.ca>
2021-04-08 14:30:52 -0300

    Combine all data type send in one object

--- MultiCommDashboards/UserControls/UC_BoolToggle.xaml.cs
+++ MultiCommDashboards/UserControls/UC_BoolToggle.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CommunicationStack.Net.Enumerations;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using WpfHelperClasses.Core;
 
 namespace MultiCommDashboards.UserControls {
 
@@ -20,19 +22,45 @@ namespace MultiCommDashboards.UserControls {
             }
         }
 
+
+        // Have data type so we know how to handle the state changed event. Always comes back as double
+
         public event EventHandler<StateChange> OnStateChange;
 
         public byte Id { get; set; } = 10;
 
+        BinaryMsgDataType dataType = BinaryMsgDataType.typeBool;
+
+
+        public void InitAsBool() {
+            this.dataType = BinaryMsgDataType.typeBool;
+            this.sliderNumeric.Collapse();
+            this.boolSlider.Show();
+        }
+
+
+        public void InitAsNumeric(BinaryMsgDataType dataType,  double step, double min, double max) {
+            this.dataType = dataType;
+            this.sliderNumeric.TickFrequency = step;
+            this.sliderNumeric.Minimum = min;
+            this.sliderNumeric.Maximum = max;
+        }
 
         public UC_BoolToggle() {
             InitializeComponent();
+            this.sliderNumeric.IsSnapToTickEnabled = true;
         }
 
-        private void sliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args) {
+        private void booSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args) {
             this.lbIdTxt.Content = args.NewValue.ToString();
+            this.lblValue.Content = ((uint)args.NewValue).ToString();
             this.OnStateChange?.Invoke(this.Id, new StateChange(this.Id, args.NewValue));
         }
 
+        private void sliderNumeric_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args) {
+
+            this.lblValue.Content = args.NewValue.ToString();
+
+        }
     }
 }
--- MultiCommDashboards/WindowObjs/MainWindow.xaml.cs
+++ MultiCommDashboards/WindowObjs/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using BluetoothCommon.Net;
 using CommunicationStack.Net.BinaryMsgs;
+using CommunicationStack.Net.Enumerations;
 using MultiCommDashboards.DependencyInjection;
+using MultiCommDashboards.UserControls;
 using MultiCommDashboards.WindowObjs.BTWins;
 using System.Windows;
 
@@ -14,7 +16,11 @@ namespace MultiCommDashboards.WindowObjs {
             InitializeComponent();
             DI.W.BT_Connected += W_BT_Connected;
             DI.W.MsgEventFloat32 += W_MsgEventFloat32;
-            this.sliderTest.OnStateChange += SliderTest_OnStateChange;
+
+            this.sliderBool.InitAsBool();
+            this.numericSlider.InitAsNumeric(BinaryMsgDataType.typeUInt8, 1, 0, 254);
+            this.sliderBool.OnStateChange += SliderBool_OnStateChange;
+            this.numericSlider.OnStateChange += NumericSlider_OnStateChange;
         }
 
 
@@ -34,6 +40,7 @@ namespace MultiCommDashboards.WindowObjs {
         }
 
         private void btnTest_Click(object sender, RoutedEventArgs e) {
+
             //MsgBoxSimple.ShowBox(this, "BLIPO");
             //MsgBoxYesNo.ShowBox(this, "Don't do it");
         }
@@ -53,12 +60,18 @@ namespace MultiCommDashboards.WindowObjs {
         }
 
         private void btnSend_Click(object sender, RoutedEventArgs e) {
+            //this.sliderTest.Init(0, 0, 1023);
+
             BinaryMsgBool mb = new BinaryMsgBool(10, true);
             DI.W.BTSend(mb.ToByteArray());
         }
 
 
-        private void SliderTest_OnStateChange(object sender, UserControls.UC_BoolToggle.StateChange state) {
+        private void NumericSlider_OnStateChange(object sender, UserControls.UC_BoolToggle.StateChange e) {
+            
+        }
+
+        private void SliderBool_OnStateChange(object sender, UC_BoolToggle.StateChange state) {
             DI.W.BTSend(new BinaryMsgBool(state.Id, state.Value).ToByteArray());
         }
 
