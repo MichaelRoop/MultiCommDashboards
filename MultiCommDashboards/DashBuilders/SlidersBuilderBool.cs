14d897da8036e18cb90af4ec3a2423cf06df8988
michael@roop.ca <michael@roop.ca>
2021-04-11 23:59:34 -0300

    Some drag drop experiments commented out

--- MultiCommDashboards/DashBuilders/SlidersBuilderBool.cs
+++ MultiCommDashboards/DashBuilders/SlidersBuilderBool.cs
@@ -45,7 +45,14 @@ namespace MultiCommDashboards.DashBuilders {
                 bt.MouseLeftButtonUp += Bt_MouseLeftButtonUp;
                 this.toggles.Add(new SliderHolder(this.nextColumn, bt)); 
                 nextColumn++;
+
+                // TODO - turn on if I can ever figure out where it is dropped
+                //bt.MouseMove += Bt_MouseMove;
+                //bt.AllowDrop = true;
                 return true;
+
+
+
             }
             return false;
         }
@@ -56,6 +63,9 @@ namespace MultiCommDashboards.DashBuilders {
             this.grid = grid;
             this.row = row;
             this.max = maxColumns;
+
+            //this.grid.Drop += Grid_Drop;
+            //this.grid.AllowDrop = true;
         }
 
 
@@ -87,6 +97,63 @@ namespace MultiCommDashboards.DashBuilders {
                 this.nextColumn = 1;
             }
         }
+
+
+        #region Drag drop experiments
+
+#if EVERFIGUREOUT
+
+        private void Bt_MouseMove(object sender, MouseEventArgs e) {
+            //https://docs.microsoft.com/en-us/dotnet/desktop/wpf/advanced/walkthrough-enabling-drag-and-drop-on-a-user-control?view=netframeworkdesktop-4.8
+            //throw new NotImplementedException();
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                UC_BoolToggle bt = sender as UC_BoolToggle;
+                if (bt != null) {
+                    //int i = 10;
+                    var data = new DataObject(typeof(UC_BoolToggle), bt);
+                    DragDrop.DoDragDrop(bt, data, DragDropEffects.Copy | DragDropEffects.Move);
+                    //https://stackoverflow.com/questions/5727391/wpf-drag-and-drop-and-data-types
+                }
+            }
+        }
+
+
+
+        private void Grid_Drop(object sender, DragEventArgs e) {
+            var obj = e.Data as DataObject;
+            UC_BoolToggle bt = obj.GetData(typeof(UC_BoolToggle)) as UC_BoolToggle;
+            if (bt != null) {
+                this.grid.Children.Remove(bt);
+                Grid g = sender as Grid;
+                // TODO how to determine the location of the drop
+                // Position relative to the grid where dropped
+                Point p = e.GetPosition(g);
+
+                // Would need to remove it from previous and set in in new area
+                //Grid.SetColumn(bt, 4);
+                //Grid.SetRow(bt, 1);
+
+                g.InvalidateArrange();
+                int col = Grid.GetColumn(bt);
+                int row = Grid.GetRow(bt);
+
+                //App.ShowMsgTitle("location", string.Format("X:{0} Y:{1}", p.X, p.Y));
+                App.ShowMsgTitle("location", string.Format("Col:{0} Row:{1}", col, row));
+
+                //Grid.SetColumn(bt, );
+                //Grid.SetRow(bt, 1);
+                // Cant get the drop location to complete work
+
+                g.Children.Add(bt);
+
+                //this.grid.Children.Add(bt);
+                this.grid.InvalidateVisual();
+            }
+        }
+
+#endif
+
+#endregion
 
     }
 }
