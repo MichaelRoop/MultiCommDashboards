5b6805ba67b136ec4139781a8b924d606bc83df4
michael@roop.ca <michael@roop.ca>
2021-04-13 12:19:01 -0300

    Added H & V progress bar controls

---
+++ MultiCommDashboards/UserControls/UC_VerticalProgressBar.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiCommDashboards.UserControls {
+    /// <summary>
+    /// Interaction logic for UC_VerticalProgressBar.xaml
+    /// </summary>
+    public partial class UC_VerticalProgressBar : UserControl {
+        public UC_VerticalProgressBar() {
+            InitializeComponent();
+        }
+    }
+}
