dd52cc210e0c4d1b99fe95769c48279e24fc1923
michael@roop.ca <michael@roop.ca>
2021-04-17 14:05:56 -0300

    Cleanup on InputBuilder

--- MultiCommDashboards/DashBuilders/InputBuilder.cs
+++ MultiCommDashboards/DashBuilders/InputBuilder.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using WpfHelperClasses.Core;
 
 namespace MultiCommDashboards.DashBuilders {
 
@@ -36,22 +35,6 @@ namespace MultiCommDashboards.DashBuilders {
 
         #endregion
 
-        #region Properties
-
-        //public List<InputControlDataModel> DataModels {
-        //    get {
-        //        List<InputControlDataModel> list = new List<InputControlDataModel>();
-        //        foreach (var control in Controls) {
-        //            InputControlDataModel dm = control.StorageInfo;
-        //            dm.Column -= COLUMN_OFFSET;
-        //            list.Add(dm);
-        //        }
-        //        return list;
-        //    }
-        //}
-
-        #endregion
-
         #region Constructors
 
         public InputBuilder() {
@@ -64,6 +47,8 @@ namespace MultiCommDashboards.DashBuilders {
         }
 
         #endregion
+
+        #region Public
 
         public void BuildConfig(DashboardConfiguration config) {
             foreach (var control in Controls) {
@@ -85,6 +70,17 @@ namespace MultiCommDashboards.DashBuilders {
             }
         }
 
+
+        public void Init(Button triggerControl, Grid grid) {
+            this.triggerControl = triggerControl;
+            this.triggerControl.Click += triggerControl_Click;
+            this.grid = grid;
+            this.max = this.grid.ColumnDefinitions.Count - COLUMN_OFFSET;
+        }
+
+        #endregion
+
+        #region Private
 
         private bool Add(int row) {
             if (this.nextColumn <= this.max) {
@@ -123,33 +119,6 @@ namespace MultiCommDashboards.DashBuilders {
         }
 
 
-        public void Init(Button triggerControl, Grid grid) {
-            //this.Reset();
-            this.triggerControl = triggerControl;
-            this.triggerControl.Click += triggerControl_Click;
-            this.grid = grid;
-            this.max = this.grid.ColumnDefinitions.Count - COLUMN_OFFSET;
-
-            //this.grid.Drop += Grid_Drop;
-            //this.grid.AllowDrop = true;
-        }
-
-
-        //public void Reset() {
-        //    if (this.triggerControl != null) {
-        //        // Need to disconnect?
-        //        this.triggerControl.Click += triggerControl_Click;
-        //    }
-
-        //    //Would need to add the trigger event also
-        //    foreach (T control in this.Controls) {
-        //        control.MouseLeftButtonUp -= this.Bt_MouseLeftButtonUp;
-        //    }
-        //    this.Controls.Clear();
-        //    this.nextColumn = COLUMN_OFFSET;
-        //}
-
-
         private void triggerControl_Click(object sender, RoutedEventArgs e) {
             if (sender is Button) {
                 Button btn = (Button)sender;
@@ -178,6 +147,7 @@ namespace MultiCommDashboards.DashBuilders {
             }
         }
 
+
         private void SetType() {
             if (typeof(T) == typeof(UC_BoolToggle)) {
                 this.inType = InputType.Bool;
@@ -190,62 +160,6 @@ namespace MultiCommDashboards.DashBuilders {
             }
         }
 
-
-        #region Drag drop experiments
-
-#if EVERFIGUREOUT
-
-        private void Bt_MouseMove(object sender, MouseEventArgs e) {
-            //https://docs.microsoft.com/en-us/dotnet/desktop/wpf/advanced/walkthrough-enabling-drag-and-drop-on-a-user-control?view=netframeworkdesktop-4.8
-            //throw new NotImplementedException();
-            if (e.LeftButton == MouseButtonState.Pressed) {
-                UC_BoolToggle bt = sender as UC_BoolToggle;
-                if (bt != null) {
-                    //int i = 10;
-                    var data = new DataObject(typeof(UC_BoolToggle), bt);
-                    DragDrop.DoDragDrop(bt, data, DragDropEffects.Copy | DragDropEffects.Move);
-                    //https://stackoverflow.com/questions/5727391/wpf-drag-and-drop-and-data-types
-                }
-            }
-        }
-
-
-
-        private void Grid_Drop(object sender, DragEventArgs e) {
-            var obj = e.Data as DataObject;
-            UC_BoolToggle bt = obj.GetData(typeof(UC_BoolToggle)) as UC_BoolToggle;
-            if (bt != null) {
-                this.grid.Children.Remove(bt);
-                Grid g = sender as Grid;
-                // TODO how to determine the location of the drop
-                // Position relative to the grid where dropped
-                Point p = e.GetPosition(g);
-
-                // Would need to remove it from previous and set in in new area
-                //Grid.SetColumn(bt, 4);
-                //Grid.SetRow(bt, 1);
-
-                g.InvalidateArrange();
-                int col = Grid.GetColumn(bt);
-                int row = Grid.GetRow(bt);
-
-                //App.ShowMsgTitle("location", string.Format("X:{0} Y:{1}", p.X, p.Y));
-                App.ShowMsgTitle("location", string.Format("Col:{0} Row:{1}", col, row));
-
-                //Grid.SetColumn(bt, );
-                //Grid.SetRow(bt, 1);
-                // Cant get the drop location to complete work
-
-                g.Children.Add(bt);
-
-                //this.grid.Children.Add(bt);
-                this.grid.InvalidateVisual();
-            }
-        }
-
-#endif
-
         #endregion
-
     }
 }
